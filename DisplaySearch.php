b1a50945d61552157e5f2c72bab210fe5bbe19d1
Daniel Hyde <33458372+DanH957@users.noreply.github.com>
2017-11-07 15:42:56 +0000

    More php files
    Displays more GUI after the user logs in.

---
+++ DisplaySearch.php
@@ -0,0 +1,6 @@
+<?
+echo "<label><b>Search Product/Barcode:</b></label><br>";
+ echo "<b><input type='text' name='search' id='search' onkeyup='search(this.value)'/></b><br>";
+ 
+			
+?>
---
+++ displaywelcome.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+echo "<span class='barcode'><h3>Barcode list:                                                                                                                        Welcome " . $_SESSION['Username'] . " Click here to <a href='' onclick='logout()' title='Logout'>Logout</a> </h3> </span>";	
+
+		
+			
+		?>
